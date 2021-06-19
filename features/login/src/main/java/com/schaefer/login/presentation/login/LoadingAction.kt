d5dd053aaea4c11ea20192b66b2a2cd983642644
Artur Schaefer <artur.schaefer@picpay.com>
2021-06-18 22:45:34 -0300

    feat: create Action

--- features/login/src/main/java/com/schaefer/login/presentation/login/LoadingAction.kt
+++
@@ -1,6 +0,0 @@
-package com.schaefer.login.presentation.login
-
-sealed class LoadingAction {
-    object NavigateToHome: LoadingAction()
-    data class ShowErrorMessage(val message: String): LoadingAction()
-}
